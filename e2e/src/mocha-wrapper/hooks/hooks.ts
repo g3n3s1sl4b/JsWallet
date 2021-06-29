ffb229db470873d51d3d02b12f5150938c0c7f55
olexandr13 <oleksandr.pelykh@gmail.com>
2021-06-29 17:32:52 +0300

    add logging of wallet url

--- e2e/src/mocha-wrapper/hooks/hooks.ts
+++ e2e/src/mocha-wrapper/hooks/hooks.ts
@@ -1,8 +1,9 @@
+import { walletURL } from '../../test-data';
 import { log } from '../../tools/logger';
 
 export const mochaHooks = {
   beforeAll(): void {
-    log.info(`Tests are running ${process.env.CI ? 'on CI' : 'locally'}`);
+    log.info(`Tests are running ${process.env.CI ? 'on CI' : 'locally'} on ${walletURL}`);
   },
 };
 
