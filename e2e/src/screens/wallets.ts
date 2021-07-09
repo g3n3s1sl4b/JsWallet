6fba8c7a135e976060e3ad8370c947875fdff02a
olexandr13 <oleksandr.pelykh@gmail.com>
2021-07-09 17:19:18 +0300

    add wallets page methods

--- e2e/src/screens/wallets.ts
+++ e2e/src/screens/wallets.ts
@@ -1,6 +1,8 @@
 import { ElementHandle, Page } from '../types';
 import { BaseScreen } from './base';
 
+type Currency = 'Bitcoin' | 'Velas' | 'Velas Native' | 'Velas EVM' | 'Litecoin';
+
 export class WalletsScreen extends BaseScreen {
   constructor(public page: Page) {
     super(page);
@@ -10,7 +12,7 @@ export class WalletsScreen extends BaseScreen {
     return (await this.page.getAttribute('div.wallet-detailed a[data-original]', 'data-original'))?.trim() || '';
   }
 
-  async selectWallet(tokenName: 'Bitcoin' | 'Velas' | 'Velas Native' | 'Velas EVM'): Promise<void> {
+  async selectWallet(tokenName: Currency): Promise<void> {
     await this.waitForWalletsDataLoaded();
     const tokenNameSelector = `div.big.wallet-item .balance.title:text(" ${tokenName}")`;
     // some time is required to load wallets and switch between them; so custom waiter is implemented
@@ -20,6 +22,10 @@ export class WalletsScreen extends BaseScreen {
       await this.page.waitForTimeout(100);
       requiredCurrencyIsALreadySelected = await this.page.isVisible(tokenNameSelector);
     }
+  }
+
+  async isWalletInWalletsList(tokenName: Currency): Promise<boolean> {
+    return this.page.isVisible(`.balance.title:text(" ${tokenName}")`);
   }
 
   async getAmountOfTokensFromOfWalletItemElement(walletElement: ElementHandle<SVGElement | HTMLElement>): Promise<string> {
@@ -37,6 +43,30 @@ export class WalletsScreen extends BaseScreen {
   async updateBalances(): Promise<void> {
     await this.page.click('.balance .button.lock');
     await this.waitForWalletsDataLoaded();
+  }
+
+  async hideWallet(): Promise<void> {
+    await this.waitForWalletsDataLoaded();
+    await this.page.click('.wallet-header .uninstall');
+    await this.waitForWalletsDataLoaded();
+  }
+
+  addWalletsPopup = {
+    open: async () => {
+      await this.page.click('.header .button.lock.mt-5');
+    },
+    add: async (tokenName: Currency) => {
+      const walletItemsList = await this.page.$$('.manage-account .settings .list .item');
+      for (let i = 0; i < walletItemsList.length; i++) {
+        const walletItem = walletItemsList[i];
+        const wallenTokenName = (await (await walletItem.$(`span.title:text("${tokenName}")`))?.textContent())?.trim();
+        const addButton = await walletItem.$('button');
+        if (wallenTokenName === tokenName) {
+          await addButton?.click();
+        }
+      }
+    }
+
   }
 
 }
