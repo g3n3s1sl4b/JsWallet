5f29b4d54ce8beec277e39a1018848267673a6bf
Corresponsible <68389402+Corresponsible@users.noreply.github.com>
2021-07-12 15:20:36 +0300

    add validation test (#146)
    * add validation test
    
    * fix validation test
    
    Co-authored-by: Ihor Karmanov <ihorkarmanov@Ihors-MacBook-Pro.local>

---
+++ e2e/src/tests/validation.test.ts
@@ -0,0 +1,47 @@
+import { test } from '@playwright/test';
+import { assert } from '../assert';
+import { Auth } from '../screens/auth';
+import { WalletsScreen } from '../screens/wallets';
+import { data, getWalletURL } from '../test-data';
+import { setupPage } from '../pw-helpers/setup-page';
+
+let walletsScreen: WalletsScreen;
+let auth: Auth;
+
+test.describe('Validation', () => {
+  test.beforeEach(async ({ page }) => {
+    setupPage(page);
+    auth = new Auth(page);
+    walletsScreen = new WalletsScreen(page);
+    await page.goto(getWalletURL({ testnet: true }));
+    await auth.loginByRestoringSeed(data.wallets.withFunds.seed);
+    await walletsScreen.waitForWalletsDataLoaded();
+  });
+
+  test('Show Invalid Address error', async ({ page }) => {
+    await page.click('#wallets-send');
+    await page.fill('#send-recipient', 'invalid data');
+    assert.isTrue(await page.isVisible('[title="Given address is not valid Velas address"]'));
+
+    await page.fill('#send-recipient', 'VAP73ARS1UXPr3jDHSzNZdss6dAudsg15U');
+    assert.isFalse(await page.isVisible('[title="Given address is not valid Velas address"]'));
+  });
+
+  test('Show Not Enough Funds error', async ({ page }) => {
+    await page.click('#wallets-send');
+    await page.fill('#send-recipient', 'VAP73ARS1UXPr3jDHSzNZdss6dAudsg15U');
+
+    await page.fill('div.amount-field .textfield[label="Send"]', '99999999');
+    //if send button is disabled, we know balance check has been finished
+    await page.waitForSelector('#send-confirm[disabled]');
+    assert.isTrue(await page.isVisible('[title="Not Enough Funds"]'));
+
+    //remove step after bug fix
+    //keeps showing error after clicking use-max if the field was not cleared
+    await page.fill('div.amount-field .textfield[label="Send"]', '');
+
+    await page.click('#send-max');
+    await page.waitForTimeout(1000);
+    assert.isFalse(await page.isVisible('[title="Not Enough Funds"]'));
+  });
+});
