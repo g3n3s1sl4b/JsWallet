7840518a0c6c9e6d7fd3c73a9d9403c96c4f7e46
olexandr13 <oleksandr.pelykh@gmail.com>
2021-07-19 15:56:13 +0300

    Tests (#151)
    * add staking accounts to test data
    
    * add staking tests
    
    * minor test fix
    
    * decrease timeouts on login; improve login with seed method
    
    * increase staking loading wait timeout
    
    * improve staking test stability
    
    * refresh staking when required
    
    * test new video; run only new acc test
    
    * run only staking
    
    * test staking test
    
    * test with timeout; decrease video size
    
    * go to wallets page and then to staking again
    
    * test
    
    * fix html report creation
    
    * increase timeout
    
    * test menu switch
    
    * fix typo
    
    * test
    
    * decrease timeout between actions
    
    * enable all staking tests
    
    * add custom waiter
    
    * test
    
    * fix test
    
    * add sleep
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * move waiting to class method
    
    * fix
    
    * run all staking tests
    
    * test fix
    
    * upd video size
    
    * add staking accounts loading to each staking test
    
    * increase stake timeout
    
    * fix
    
    * decrease wait timeout
    
    * increase default wait timeout
    
    * add blockchain to staking tests
    
    * update package-lock
    
    * extend staking class
    
    * add blockchain to staking tests
    
    * minor cahnges in staking tests
    
    * run all tests

--- e2e/src/tests/staking.test.ts
+++ e2e/src/tests/staking.test.ts
@@ -5,10 +5,12 @@ import { Auth } from '../screens/auth';
 import { StakingScreen } from '../screens/staking';
 import { WalletsScreen } from '../screens/wallets';
 import { data, getWalletURL } from '../test-data';
+import { VelasNative } from '@velas/velas-chain-test-wrapper';
 
 let auth: Auth;
 let walletsScreen: WalletsScreen;
 let stakingScreen: StakingScreen;
+const velasNative = new VelasNative();
 
 test.describe('Staking', () => {
   test.beforeEach(async ({ page }) => {
@@ -22,21 +24,41 @@ test.describe('Staking', () => {
   });
 
   test.describe('Actions', () => {
+    const stakingAmount = 1;
+
     test('Create staking account', async ({ page }) => {
+      const VLXNativeAddress = '59vpQgPoDEhux1G84jk6dbbARQqfUwYtohLU4fgdxFKG';
+
       const initialAmountOfStakingAccounts = await stakingScreen.getAmountOfStakes('to_delegate');
+      const stakingAccountAddresses = await stakingScreen.getStakingAccountsAddresses();
+      const initialWalletBalance = Number((await velasNative.getBalance(VLXNativeAddress)).VLX.toFixed(0));
+
       await page.click('" Create Account"');
-      await page.fill('.input-area input', '1');
+      await page.fill('.input-area input', String(stakingAmount));
       await page.click('#prompt-confirm');
       await page.waitForSelector('" Account created and funds deposited"', { timeout: 10000 });
       await page.click('#notification-close');
 
       // for some reason new stake does not appear in the list immediately
       const finalAmountOfStakingAccounts = await stakingScreen.waitForStakesAmountUpdated(initialAmountOfStakingAccounts, 'to_delegate');
-      assert.equal(finalAmountOfStakingAccounts, initialAmountOfStakingAccounts + 1);
+      assert.equal(finalAmountOfStakingAccounts, initialAmountOfStakingAccounts + stakingAmount);
+
+      const newlyAddedStakingAccountAddress = (await stakingScreen.getStakingAccountsUpdate(stakingAccountAddresses))?.added;
+      if (!newlyAddedStakingAccountAddress) throw new Error('No new staking account appears in the list');
+
+      // assert VLXNative balance decreases on staking amount
+      const finalWalletBalance = Number((await velasNative.getBalance(VLXNativeAddress)).VLX.toFixed(0));
+      assert.equal(finalWalletBalance, initialWalletBalance - stakingAmount);
+
+      // check newly created staking account on blockchain
+      await stakingScreen.makeSureStakingAccIsCreatedAndNotDelegated(newlyAddedStakingAccountAddress);
+      assert.equal((await velasNative.getBalance(newlyAddedStakingAccountAddress)).VLX.toFixed(0), String(stakingAmount));
     });
 
     test('Delegate stake', async ({ page }) => {
       const initialAmountOfDelegatedStakes = await stakingScreen.getAmountOfStakes('to_undelegate');
+      const stakeAccountAddress = await stakingScreen.getFirstStakingAccountAddressFromTheList('to_delegate');
+
       await stakingScreen.clickDelegate();
       assert.isFalse(await page.isVisible('#choosen-pull'));
       await page.click('.staking-content.delegate button');
@@ -46,11 +68,18 @@ test.describe('Staking', () => {
       await page.click('" Ok"');
       const finalAmountOfDelegatedStakes = await stakingScreen.waitForStakesAmountUpdated(initialAmountOfDelegatedStakes, 'to_undelegate');
       assert.equal(finalAmountOfDelegatedStakes, initialAmountOfDelegatedStakes + 1);
+
+      const stakeAccOnBlockchain = await velasNative.getStakeAccount(stakeAccountAddress);
+      assert.equal(stakeAccOnBlockchain.active, 0);
+      assert.equal(stakeAccOnBlockchain.inactive, stakingAmount * 10 ** 9);
+      assert.equal(stakeAccOnBlockchain.state, 'activating');
     });
 
     test('Undelegate stake', async ({ page }) => {
       const initialToUndelegateStakesAmount = await stakingScreen.getAmountOfStakes('to_undelegate');
       const initialToDelegateStakesAmount = await stakingScreen.getAmountOfStakes('to_delegate');
+      const stakeAccountAddress = await stakingScreen.getFirstStakingAccountAddressFromTheList('to_delegate');
+
       await stakingScreen.clickUndelegate();
       await page.click('" Confirm"');
       await page.waitForSelector('" Funds undelegated successfully"');
@@ -58,10 +87,18 @@ test.describe('Staking', () => {
       const finalToUndelegateStakesAmount = await stakingScreen.waitForStakesAmountUpdated(initialToUndelegateStakesAmount, 'to_undelegate');
       assert.equal(finalToUndelegateStakesAmount, initialToUndelegateStakesAmount - 1, 'Amount of stakes to undelegate has not changed after undelegation');
       assert.equal(await stakingScreen.getAmountOfStakes('to_delegate'), initialToDelegateStakesAmount + 1, 'Amount of stakes to withdraw has not changed after undelegation');
+
+      const stakeAccOnBlockchain = await velasNative.getStakeAccount(stakeAccountAddress);
+      assert.equal(stakeAccOnBlockchain.active, 0);
+      assert.equal(stakeAccOnBlockchain.inactive, stakingAmount * 10 ** 9);
+      assert.equal(stakeAccOnBlockchain.state, 'inactive');
     });
 
     test('Withdraw stake', async ({ page }) => {
+      const stakingAccountAddresses = await stakingScreen.getStakingAccountsAddresses();
       const initialAmountOfStakingAccounts = await stakingScreen.getAmountOfStakes('all');
+      const stakeAccountAddress = await stakingScreen.getFirstStakingAccountAddressFromTheList('to_delegate');
+
       await stakingScreen.selectAccount('to_delegate');
       await page.click('button span:text(" Withdraw")');
       await page.click('" Confirm"');
@@ -70,6 +107,10 @@ test.describe('Staking', () => {
       const finalAmountOfStakingAccounts = await stakingScreen.waitForStakesAmountUpdated(initialAmountOfStakingAccounts, 'all');
 
       assert.equal(finalAmountOfStakingAccounts, initialAmountOfStakingAccounts - 1);
+
+      await stakingScreen.makeSureStakingAccountDoesNotExist(stakeAccountAddress);
+      const withdrawedStakeAccountAddress = (await stakingScreen.getStakingAccountsUpdate(stakingAccountAddresses))?.removed;
+      assert.equal(withdrawedStakeAccountAddress, stakeAccountAddress);
     });
 
     // test('Split stake', async ({ page }) => {
@@ -93,7 +134,6 @@ test.describe('Staking', () => {
   //   });
   // });
 });
-
 
 // page.on('response', async (response) => {
 //   console.log('<<', response.status(), response.url());
---
+++ e2e/src/tools/helpers.ts
@@ -0,0 +1,23 @@
+export const helpers = {
+  getArraysDiff: (a1: any[], a2: any[]) => {
+    const a: any[] = [], diff = [];
+
+    for (var i = 0; i < a1.length; i++) {
+      a[a1[i]] = true;
+    }
+
+    for (var i = 0; i < a2.length; i++) {
+      if (a[a2[i]]) {
+        delete a[a2[i]];
+      } else {
+        a[a2[i]] = true;
+      }
+    }
+
+    for (var k in a) {
+      diff.push(k);
+    }
+
+    return diff;
+  }
+}
