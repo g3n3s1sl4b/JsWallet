a8e5b0cc55e6772aced7d62ca877df83514454fd
Ihor Karmanov <gariq.lg@gmail.com>
2021-07-23 13:30:14 +0300

    merge

--- e2e/src/screens/wallets.ts
+++ e2e/src/screens/wallets.ts
@@ -10,20 +10,25 @@ export class WalletsScreen extends BaseScreen {
     super(page);
   }
 
+  async refresh(): Promise<void> {
+    await this.page.click('.balance .button');
+  }
+
   async getWalletAddress(): Promise<string> {
     return (await this.page.getAttribute('div.wallet-detailed a[data-original]', 'data-original'))?.trim() || '';
   }
 
-  async selectWallet(tokenName: Currency): Promise<void> {
+  async selectWallet(tokenName: Currency ): Promise<void> {
     await this.waitForWalletsDataLoaded();
-    const tokenNameSelector = `div.big.wallet-item .balance.title:text(" ${tokenName}")`;
+    const tokenNameSelector = `div.big.wallet-item .balance.title:text-matches("^ ${tokenName}$")`;
     // some time is required to load wallets and switch between them; so custom waiter is implemented
     let requiredCurrencyIsALreadySelected = await this.page.isVisible(tokenNameSelector);
     while (!requiredCurrencyIsALreadySelected) {
       await this.page.click(`.balance.title:text(" ${tokenName}")`);
-      await this.page.waitForTimeout(100);
+      await this.page.waitForTimeout(3000);
       requiredCurrencyIsALreadySelected = await this.page.isVisible(tokenNameSelector);
     }
+    log.debug(`${tokenName} was selected`);
   }
 
   async getWalletsBalances(): Promise<Balances> {
@@ -97,7 +102,70 @@ export class WalletsScreen extends BaseScreen {
           await addButton?.click();
         }
       }
-    },
+    }
+  }
 
+  async swapTokens(swapFromToken: Currency, swapToToken: Currency, transactionAmount: number): Promise<void> {
+    if (swapFromToken === swapToToken){
+      throw TypeError('You can\'t swap to the same token you are swapping from');
+    }
+
+    if (swapFromToken === 'Velas EVM' || swapToToken === 'Velas EVM'){
+      await this.addWalletsPopup.open();
+      await this.addWalletsPopup.add('Velas EVM');
+    }
+
+    if (swapFromToken !== 'Velas'){
+      await this.selectWallet(swapFromToken);
+    }
+
+    await this.swap.click();
+
+    await this.swap.chooseDestinationNetwork(swapToToken);
+
+    await this.swap.confirm(String(transactionAmount));
+  }
+
+  private swap = {
+    click: async() => {
+      await this.page.click('.with-swap #wallet-swap');
+      await this.page.waitForSelector('.network-slider');
+    },
+    fill: async() => {
+      await this.page.click('.with-swap #wallet-swap');
+      await this.page.waitForSelector('.network-slider');
+    },
+    chooseDestinationNetwork: async(swapToToken: Currency) => {
+      let chosenNetwork = await this.page.getAttribute('.change-network', 'value');
+      while (chosenNetwork !== swapToToken.toUpperCase()){
+        await this.page.click('.network-slider .right');
+        chosenNetwork = await this.page.getAttribute('.change-network', 'value');
+      }
+    },
+    confirm: async(transactionAmount: string) => {
+      await this.page.fill('div.amount-field .input-area input[label="Send"]', transactionAmount);
+      await this.page.click('#send-confirm');
+      await this.page.click('#confirmation-confirm', {timeout: 10000});
+    },
+  }
+
+  async getLastTxSignatureInHistory(): Promise<string> {
+    await this.page.click('[datatesting="transaction"] div.more', {timeout: 15000});
+
+    const lastTxSignatureElementSelector = '[datatesting="transaction"] .tx-middle .txhash a[data-original]';
+    const lastTxSignature = (await this.page.getAttribute(lastTxSignatureElementSelector, 'data-original'))?.trim();
+    if (!lastTxSignature) throw new Error(`Cannot get transaction signature from element with selector ${lastTxSignatureElementSelector}`)
+    await this.page.click('[datatesting="transaction"] div.more');
+    return lastTxSignature;
+  }
+
+  async waitForTxHistoryUpdated(previousTxSignature: string): Promise<void> {
+    let currentTxSignature = await this.getLastTxSignatureInHistory();
+    while (currentTxSignature === previousTxSignature) {
+      log.warn('History hasn\'t been updated. Wait and refresh the history...');
+      await this.page.waitForTimeout(2000);
+      await this.refresh();
+      currentTxSignature = await this.getLastTxSignatureInHistory();
+    }
   }
 }
