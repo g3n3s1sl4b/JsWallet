7b6290800680c2e9bf5149ab216a58821d188ac1
corresponsible <gariq.lg@gmail.com>
2021-07-28 00:31:09 +0300

    remove ETH test from unstable folder

--- e2e/src/tests/unstable/transactions.test.ts
+++ e2e/src/tests/unstable/transactions.test.ts
@@ -50,24 +50,6 @@ test.describe('Transactions >', () => {
     assert.isBelow(senderFinalBalance.VLX, senderInitialBalance.VLX - transactionAmount, 'Final sender balance is not below the initial sender balance');
   });
 
-  test('Send ETH', async ({ page }) => {
-    await walletsScreen.addWalletsPopup.open();
-    await walletsScreen.addWalletsPopup.add('Ethereum');
-    await walletsScreen.waitForWalletsDataLoaded();
-    
-    const transactionAmount = 0.00001;
-
-    await walletsScreen.selectWallet('Ethereum');
-    await page.click('#wallets-send');
-    await page.fill('#send-recipient', '0xb322f01cb6a191974e7291600a4dc1b46f00f752'); //accound with index 2
-    await page.type('div.amount-field input[label="Send"]', String(transactionAmount));
-    await page.click('#send-confirm');
-    await page.click('#confirmation-confirm');
-
-    const txSignatureLink = String(await page.getAttribute('.sent .text a', 'href'));
-    assert.isTrue(txSignatureLink.includes('https://ropsten.etherscan.io/'));
-  });
-
   test('Send BTC', async ({ page }) => {    
     const transactionAmount = 0.00001;
 
